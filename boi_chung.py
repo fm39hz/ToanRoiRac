01a0ba73bea18751e3ff8bf71d1393bd18cdf78f
fm39hz-desktop <hitpoint2k3@gmail.com>
2024-05-20 08:26:12 +0700

    update

--- boi_chung.py
+++ boi_chung.py
@@ -1,35 +1,28 @@
-def find_numbers_divisible(start, end, divisor):
-    result = []
-    for num in range(start, end + 1):
-        if num % divisor == 0:
-            result.append(num)
-    write_result(str(divisor), str(result))
-
-
-def find_numbers_same_divisible(start, end, divisors):
-    result = set()
-
-    for num in range(start, end + 1):
-        is_common_multiple = all(num % divisor == 0 for divisor in divisors)
-        if is_common_multiple:
-            result.add(num)
-
-    write_result("_".join(str(divisor) for divisor in divisors), str(result))
-
-
-def write_result(number, results):
-    for result in results:
-        with open(f"{number}_result.txt", "w") as f:
-            f.write(result)
-
-
 start = 22218782
 end = 67220244
-divisors = [297, 3969, 41503]
+nums = [297, 3969, 41503]
 
-for divisor in divisors:
-    find_numbers_divisible(start, end, divisor)
-    find_numbers_same_divisible(start, end, [divisors[0], divisors[1]])
-    find_numbers_same_divisible(start, end, [divisors[1], divisors[2]])
-    find_numbers_same_divisible(start, end, [divisors[0], divisors[2]])
-    find_numbers_same_divisible(start, end, divisors)
+print_divisors = ",".join(str(num) for num in nums)
+
+
+def is_true(i, divisor):
+    return i % divisor == 0
+
+
+def count_divisor():
+    counter_a = 0
+    counter_b = 0
+    counter_c = 0
+    for i in range(start, end):
+        if is_true(i, nums[0]) and is_true(i, nums[1]) and is_true(i, nums[2]):
+            counter_a += 1
+        elif is_true(i, nums[0]) or is_true(i, nums[1]):
+            counter_b += 1
+        elif is_true(i, nums[0]) or is_true(i, nums[1]) or is_true(i, nums[2]):
+            counter_c += 1
+    print(f" Số các số chia hết cho cả {print_divisors} là: {counter_a}")
+    print(f" Số các số chia hết cho hoặc {nums[0]} hoặc {nums[1]} là: {counter_b}")
+    print(f" Số các số chia hết cho ít nhất một trong {print_divisors} là: {counter_c}")
+
+
+count_divisor()
--- tim_so_xau_nhi_phan.py
+++ tim_so_xau_nhi_phan.py
@@ -1,6 +1,27 @@
 import math
 
 
+def count_binary_string_with_length(length):
+    return 2**length
+
+
+print(f"Số xâu nhị phân độ dài 27 là: {count_binary_string_with_length(27)}")
+print(
+    f"Số xâu nhị phân độ dài 25 bắt đầu bằng xâu 001 là: {count_binary_string_with_length(22)}"
+)
+
+
+def count_binary_strings_at_least_some_bin(length):
+    total_strings = 2**length
+    sum_no_zeros = 1 + length + sum(math.comb(length, i) for i in range(2, 7))
+    result = total_strings - sum_no_zeros
+    return result
+
+
+result = count_binary_strings_at_least_some_bin(31)
+print(f"Số lượng xâu nhị phân độ dài 31 có chứa ít nhất 7 bit 0 là: {result}")
+
+
 def binomial_coefficient(n, k):
     return math.comb(n, k)
 
@@ -10,3 +31,7 @@ k = 7
 result = binomial_coefficient(n, k)
 
 print(f"Số lượng xâu nhị phân độ dài {n} và chứa đúng {k} bit 1 là: {result}")
+result = binomial_coefficient(32, 11)
+print(
+    f"Số lượng xâu nhị phân độ dài 32 và chứa đúng 11 bit 0 và sau mỗi bit 0 là bit 1 là: {result}"
+)
